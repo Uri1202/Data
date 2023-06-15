092a91919737c5de72516e2afaa57c5ee0bf1c30
Uri1202 <109200281+Uri1202@users.noreply.github.com>
2023-06-15 22:34:19 +0900

    Dice game: initial revision

--- ex01.java
+++ ex01.java
@@ -1,1 +1,18 @@
-a
+import java.util.Random;
+
+public class DiceGame {
+    public static void main(String[] args) {
+        rollDice();
+    }
+
+    public static void rollDice() {
+        System.out.println("Rolling dice...");
+        Random random = new Random();
+        int die1 = random.nextInt(6)+1;
+        int die2 = random.nextInt(6)+1;
+
+        System.out.println("Die 1: " + die1);
+        System.out.println("Die 2: " + die2);
+        System.out.println("Total value: " + (die1 + die2));
+    }
+}
