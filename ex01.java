38a850614f14644362c7d16f760f52209b2063d3
Uri1202 <109200281+Uri1202@users.noreply.github.com>
2023-06-15 22:59:36 +0900

    Won/lost message modified

--- ex01.java
+++ ex01.java
@@ -7,25 +7,25 @@ public class DiceGame {
     }
 
     public static void rollDice() {
-        System.out.println("Rolling dice...");
         Random random = new Random();
         int die1 = random.nextInt(6) + 1;
         int die2 = random.nextInt(6) + 1;
         int totalValue = die1 + die2;
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+
+        System.out.println("Rolling dice...");
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
         System.out.println("Total value: " + (die1 + die2));
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What is your name? ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-
         if (totalValue > 7) {
-            System.out.println("You won!");
+            System.out.println(name+" won!");
         } else {
-            System.out.println("You lost.");
+            System.out.println(name+" lost.");
         }
     }
 }
