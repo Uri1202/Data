a5b3c7b3a1492281d2a6db46d5676c5b92ca150d
Uri1202 <109200281+Uri1202@users.noreply.github.com>
2023-06-15 22:39:16 +0900

    Greetings added

--- ex01.java
+++ ex01.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
@@ -13,6 +14,10 @@ public class DiceGame {
 
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
-        System.out.println("Total value: " + (die1 + die2));
+        System.out.println("Total value: " + (die1 + die2));\
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
